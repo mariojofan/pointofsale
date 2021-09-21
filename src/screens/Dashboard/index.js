a8f35dbae7815e54905bba33825f9bf27a0af33a
unknown <you@example.com>
2021-09-21 18:40:54 +0800

    Update 2

--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet, TextInput } from 'react-native';
+import { Text, View, StyleSheet, TextInput, TouchableOpacity, Card } from 'react-native';
 
 const Navbar = () => {
     return (
@@ -18,6 +18,7 @@ const Navbar = () => {
                     <Text style={styles.photo}></Text>
                 </View>
             </View>
+            <View style={{backgroundColor: '#E1E1E1', height: 1}}></View>
         </View>
     )
 }
@@ -33,11 +34,47 @@ const SearhingBar = () => {
     )
 }
 
+const Category = () => {
+    return (
+        <View>
+            <TouchableOpacity>
+                <View style={styles.parentContainer}>
+                    <Text style={styles.all}>All</Text>
+                    <Text style={styles.all}>Makanan</Text>
+                    <Text style={styles.all}>Minuman</Text>
+                    <Text style={styles.all}>Snack</Text>
+                    <Text style={styles.all}>Paket</Text>
+                </View>
+            </TouchableOpacity>
+            <View style={styles.line}>
+                <View style={styles.line2}>
+
+                </View>
+            </View>
+        </View>
+    )
+}
+
+const Card = () => {
+    return (
+        <View>
+            <View>
+                <Text>Food</Text>
+                <Card>
+                    <Text>dfdf</Text>
+                </Card>
+            </View>
+        </View>
+    )
+}
+
 const Dashboard = () => {
     return (
         <View>
             <Navbar />
             <SearhingBar />
+            <Category />
+            <Card />
         </View>
     )
 }
@@ -52,11 +89,11 @@ const styles = StyleSheet.create({
 
     // Navbar
     header: {
-        backgroundColor: 'gray',
+        backgroundColor: 'white',
         alignItems: 'center',
         height: 51,
         flexDirection: 'row',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
     },
     icon1: {
         backgroundColor: 'black',
@@ -86,6 +123,7 @@ const styles = StyleSheet.create({
         borderRadius: 20,
         marginLeft: 7
     },
+
     //  Seach bar
     bar: {
         backgroundColor: '#F7F7FC',
@@ -109,6 +147,26 @@ const styles = StyleSheet.create({
     },
     containerChild2: {
         position: 'relative',
+    },
+
+    // Category
+    parentContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginTop: 15
+    },
+    all: {
+        fontSize: 14
+    },
+    line: {
+        backgroundColor: '#DBD7F4',
+        height: 4,
+        marginTop: 15,
+    },
+    line2: {
+        backgroundColor: '#39A2DB',
+        width: 70,
+        height: 4
     }
 
 })
